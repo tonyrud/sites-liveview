188275edc40e3150e611286043ed782e97327ce3
tonyrud <tonrudny@gmail.com>
2020-12-19 15:51:34 -0500

    credo updates

--- mix.exs
+++ mix.exs
@@ -74,6 +74,7 @@ defmodule Demo.MixProject do
 
   defp dialyzer do
     [
+      plt_add_apps: [:mix, :ex_unit],
       plt_core_path: "priv/plts",
       plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
     ]
