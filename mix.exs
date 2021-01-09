34baf3094c923c51f13482ae6d2275af9b251c23
Anthony Rudny <tonrudny@gmail.com>
2021-01-09 09:52:21 -0500

    Docker build testing (#12)
    * dockerfile updates
    
    * docker multi stage testing

--- mix.exs
+++ mix.exs
@@ -89,8 +89,8 @@ defmodule Demo.MixProject do
   defp dialyzer do
     [
       plt_add_apps: [:mix, :ex_unit],
-      plt_core_path: "priv/plts",
-      plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      plt_core_path: "_build",
+      plt_file: {:no_warn, "_build/dialyzer.plt"}
     ]
   end
 end
